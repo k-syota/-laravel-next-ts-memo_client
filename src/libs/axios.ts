f3d423fcd19f0257bbabed1023f86c433e670dfc
Syota Nishimura <syota4723@gmail.com>
2023-05-10 23:21:38 +0900

    :adhesive_bandage: urlの修正

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,11 +1,12 @@
-import Axios from 'axios'
- 
+import Axios from "axios";
+
 const axios = Axios.create({
-    baseURL: 'http://localhost:8000',
-    headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-    },
-    withCredentials: true,
-})
- 
-export default axios
+  // baseURL: 'http://localhost:8000',
+  baseURL: "https://laravel-next-ts-memo-api-rlz8.vercel.app",
+  headers: {
+    "X-Requested-With": "XMLHttpRequest",
+  },
+  withCredentials: true,
+});
+
+export default axios;
